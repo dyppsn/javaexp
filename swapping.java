226a864b38bc11378b13bbb8420a63925cdbf131
dyppsn <n23961448@gmail.com>
2025-11-04 01:14:41 +0530

    Create swapping.java

---
+++ swapping.java
@@ -0,0 +1,28 @@
+Swapping 
+
+import java.util.Scanner;
+
+public class SwapNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        System.out.println("\nBefore swapping:");
+        System.out.println("a = " + a + ", b = " + b);
+
+        // Swapping using temporary variable
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("\nAfter swapping:");
+        System.out.println("a = " + a + ", b = " + b);
+
+        sc.close();
+    }
+}
